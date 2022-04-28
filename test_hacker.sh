21da89c965a40355c80407ecbea34e6976bf5e92
PacoRodriguezLozano <pacorl97@outlook.es>
2022-04-28 10:53:56 +0200

    no funcionaba por que creaba una carpeta dentro de ref_repo. Solucionado

--- test_hacker.sh
+++ test_hacker.sh
@@ -1,9 +1,6 @@
 #!bin/bash
-mkdir ref_repo
-cd ./ref_repo
 read equipo_confinados
-git clone $equipo_confinados
-cd ..
+git clone $equipo_confinados ./ref_repo
 for i in *.out
 do
 	diff -q $i ./ref_repo/$i
